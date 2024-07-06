9475ce984571de740e5663b7b71f1e0b2f88bbf6
MohamTahaB <bayoumimt@gmail.com>
2024-07-06 14:38:47 +0200

    init game

---
+++ structs/game/game.cpp
@@ -0,0 +1,18 @@
+#include "game.hpp"
+
+Game::Game()
+    : piece(Piece()), isPieceSet(false),
+      board(std::array<std::array<bool, 10>, 24>{}) {}
+
+Game::~Game() { piece.~Piece(); }
+
+void Game::setPiece(PieceGrid pg) {
+  piece.setPieceGrid(pg);
+
+  // TODO! add funcs to check the nax up, left, right most points to know how to
+  // position the reference, which will be now simply <0,0>
+
+  piece.setPosition({0, 0});
+}
+
+bool Game::inspectState() {}
---
+++ structs/game/game.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "piece.hpp"
+#include <array>
+
+class Game {
+public:
+  Game();
+  ~Game();
+
+  bool inspectState();
+  void movePiece(char c);
+  void setPiece(PieceGrid);
+
+private:
+  std::array<std::array<bool, 10>, 24> board;
+  Piece piece;
+  bool isPieceSet;
+};
